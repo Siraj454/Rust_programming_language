dfa3204a9f355e6e6225896c508bb9f6558d54ef
MuhammadSirajAbbasi <64827860+Siraj454@users.noreply.github.com>
2020-06-21 10:27:18 +0500

    Complete Demo app

--- ch_6_match/src/main.rs
+++ ch_6_match/src/main.rs
@@ -22,4 +22,33 @@ fn main() {
     println!("The Value of Red color note is {},blue is {} green is {} and Dark is {}",
     red_note,blue_note,green_note,dark_note);
     
+
+
+      let s1 = "hello dolly".to_string();
+      dump(&s1);
+      println!("s1 {}", s1);
+     
+      println!("Parsing String to Number");//remember data type should be same
+      let parsed: i32 = "5".parse().unwrap();
+      let turbo_parsed = "10".parse::<i32>().unwrap();
+  
+      let sum = parsed + turbo_parsed;
+      println!("Sum: {:?}", sum);
+      
+
+      println!("For loop check ");
+      let a= 5;
+      let mut b=0;// mut is used to take value outSIde
+      for i in 0..a{
+         b=b+1;
+        println!("value of b in scope is {}",b)
+      }
+     println!(" ***value of b outScope is {}",b);
+
+  }
+  
+
+
+fn dump(s: &String) {
+  println!("{}", s);
 }
---
+++ ch_6_match/target/rls/debug/deps/ch_6_match-35ec1d50389b78d6.d
@@ -0,0 +1,5 @@
+/home/siraj/Documents/iot/Rust_programming_language/ch_6_match/target/rls/debug/deps/ch_6_match-35ec1d50389b78d6.rmeta: src/main.rs
+
+/home/siraj/Documents/iot/Rust_programming_language/ch_6_match/target/rls/debug/deps/ch_6_match-35ec1d50389b78d6.d: src/main.rs
+
+src/main.rs:
